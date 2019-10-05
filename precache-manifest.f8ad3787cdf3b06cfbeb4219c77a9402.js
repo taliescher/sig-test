92e5b74fde3c38cb068ed13319dfb6eb591f07bc
Tali Escher <hey@tali.dev>
2019-10-05 20:18:21 +0200

    Updates

---
+++ precache-manifest.f8ad3787cdf3b06cfbeb4219c77a9402.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3abfe2e11494ab64fe344f43e450ff",
+    "url": "/sig-test/index.html"
+  },
+  {
+    "revision": "6126e55b0c8b51e59b08",
+    "url": "/sig-test/static/css/main.a8d3f39b.chunk.css"
+  },
+  {
+    "revision": "83f2160b6663d0524668",
+    "url": "/sig-test/static/js/2.9c8af9a4.chunk.js"
+  },
+  {
+    "revision": "6126e55b0c8b51e59b08",
+    "url": "/sig-test/static/js/main.eacd6ec8.chunk.js"
+  },
+  {
+    "revision": "7ea1ed4e4ebcf14093e6",
+    "url": "/sig-test/static/js/runtime~main.99473b71.js"
+  }
+]);
